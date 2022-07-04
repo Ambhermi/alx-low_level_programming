f96b76d5dd4a3232aaee4f2b312fa059826ce18f
Ambhermi <106977959+Ambhermi@users.noreply.github.com>
2022-07-04 17:36:15 -0300

    Create 6-size.c

---
+++ 6-size.c
@@ -0,0 +1,17 @@
+/c/0x00$ gcc 6-size.c -m32 -o size32 2> /tmp/32
+/c/0x00$ gcc 6-size.c -m64 -o size64 2> /tmp/64
+/c/0x00$ ./size32
+Size of a char: 1 byte(s)
+Size of an int: 4 byte(s)
+Size of a long int: 4 byte(s)
+Size of a long long int: 8 byte(s)
+Size of a float: 4 byte(s)
+/c/0x00$ ./size64
+Size of a char: 1 byte(s)
+Size of an int: 4 byte(s)
+Size of a long int: 8 byte(s)
+Size of a long long int: 8 byte(s)
+Size of a float: 4 byte(s)
+/c/0x00$ echo $?
+0
+/c/0x00$
